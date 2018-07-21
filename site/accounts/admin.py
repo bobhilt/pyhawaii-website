7447d32e7257cd17a7c5b4dc7f49672a8f0f7229
jeffrey k eliasen <jeff@jke.net>
2018-07-21 09:22:50 -1000

    admin tweaks for searching/sorting

--- site/accounts/admin.py
+++ site/accounts/admin.py
@@ -11,6 +11,7 @@ class UserAdmin(admin.ModelAdmin):
 
     list_display = ('username', 'email', 'is_active', 'is_staff', 'is_service', 'is_superuser')
     list_filter = ('is_active', 'is_superuser', 'is_staff', 'is_service')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
 
     readonly_fields = ('date_joined', 'last_login')
 
